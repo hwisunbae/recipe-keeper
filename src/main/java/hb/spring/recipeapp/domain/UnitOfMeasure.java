bcdf5d68c8769185e527d0875128500397c3f06e
Hailey Bae <hwisunbae.hb@gmail.com>
2020-03-19 18:40:43 +0900

    complete JPA relationships

---
+++ src/main/java/hb/spring/recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package hb.spring.recipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
