aeac85333ede7f0202bde5a756e3e367313a3fbb
Hailey Bae <hwisunbae.hb@gmail.com>
2020-03-27 18:15:52 +0900

    added for show ingredient

--- src/main/java/hb/spring/recipeapp/converters/IngredientToCommand.java
+++ src/main/java/hb/spring/recipeapp/converters/IngredientToCommand.java
@@ -26,8 +26,12 @@ public class IngredientToCommand implements Converter<Ingredient, IngredientComm
     public IngredientCommand convert(Ingredient ingredient) {
         if (ingredient == null)
             return null;
+
         final IngredientCommand ingredientCommand = new IngredientCommand();
         ingredientCommand.setId(ingredient.getId());
+        if (ingredient.getRecipe() != null )
+            ingredientCommand.setRecipeId(ingredient.getRecipe().getId());
+
         ingredientCommand.setAmount(ingredient.getAmount());
         ingredientCommand.setDescription(ingredient.getDescription());
         ingredientCommand.setUnitOfMeasureCommand(converter.convert(ingredient.getUom()));
